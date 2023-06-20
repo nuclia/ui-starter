1296934a7a52c08811a752489bf4b9aad6f0e9af
mpellerin42 <mpellerin42@gmail.com>
2023-06-20 15:43:39 +0200

    Create a new page for Search API results example

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,54 @@
+<script>
+  import {page} from '$app/stores';
+</script>
+
+<nav>
+  <a href="/" class:active={$page.url.pathname === '/'}>Main widgets</a>
+  <a href="/api-results" class:active={$page.url.pathname === '/api-results'}>Search API results</a>
+</nav>
+
+<main>
+  <slot></slot>
+</main>
+
+
+<style lang="scss">
+
+  main,
+  nav {
+    font-family: sans-serif;
+  }
+
+  main {
+    max-width: 100%;
+    margin: 0 auto;
+    padding: 1em;
+  }
+
+  nav {
+    background: #eeebff;
+    display: flex;
+    transition: background 0.16s ease-in-out;
+
+    a {
+      background: #eeebff;
+      color: #2600ff;
+      transition: color 0.16s ease-in-out, background 0.16s ease-in-out;
+      padding: 1rem;
+      text-decoration: none;
+
+      &:hover {
+        color: #1f00cc;
+      }
+
+      &:active {
+        color: #12007a;
+      }
+
+      &.active {
+        background: #fff;
+        color: #000;
+      }
+    }
+  }
+</style>
---
+++ src/routes/api-results/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { NucliaSearchResults } from '@nuclia/ui';
+</script>
+
+<header>
+  <div class="main-description">
+    <h2>Search API results</h2>
+    <p>
+      On this example, we loaded some search results directly from the API (ie. without using Nuclia
+      <code>SearchBar</code> widget).
+      <br>Then we display them in <code>NucliaSearchResults</code> widget.
+    </p>
+
+  </div>
+
+</header>
+
+<NucliaSearchResults/>
+
+
+<style lang="scss">
+  @import "../common";
+</style>
