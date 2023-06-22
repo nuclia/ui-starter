2d5043df4dda578503af9c1ce6c1cd15792449b1
Mat Pellerin <mpellerin42@gmail.com>
2023-06-22 11:21:10 +0200

    Add custom error page (#2)
    * Add custom error page
    
    * Fix typo
    
    Co-authored-by: Eric BREHAULT <ebrehault@gmail.com>
    
    ---------
    
    Co-authored-by: Eric BREHAULT <ebrehault@gmail.com>

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script>
+  import {page} from '$app/stores';
+</script>
+
+<main class="main-description">
+  <h1>{$page.status} {$page.error.message}</h1>
+
+  {#if $page.status >= 500}
+    <p>This can be caused by outdated Nuclia dependencies.</p>
+    <p>To solve the problem, try to run <code>npm run missdev</code> and then reload the page.</p>
+  {:else if $page.status === 404}
+    <p>Seems like you’re trying to access a page that doesn’t exist…</p>
+  {/if}
+</main>
+
+<style lang="scss">
+  @import "common";
+
+  code {
+    background: #e6e6e6;
+    padding: 2px 4px;
+  }
+</style>
