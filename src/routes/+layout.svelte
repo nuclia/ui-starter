8922c6b0cd1afbc5842cbe9aba50c8a3f0f796e0
Mat Pellerin <mpellerin42@gmail.com>
2023-06-22 17:19:59 +0200

    Dark mode page example (#3)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,10 @@
   import {page} from '$app/stores';
 </script>
 
-<nav>
+<nav class:dark={$page.url.pathname === '/dark-theme'}>
   <a href="/" class:active={$page.url.pathname === '/'}>Main widgets</a>
   <a href="/api-results" class:active={$page.url.pathname === '/api-results'}>Search API results</a>
+  <a href="/dark-theme" class:active={$page.url.pathname === '/dark-theme'}>Dark theme</a>
 </nav>
 
 <main>
@@ -30,25 +31,61 @@
     display: flex;
     transition: background 0.16s ease-in-out;
 
+
     a {
-      background: #eeebff;
-      color: #2600ff;
       transition: color 0.16s ease-in-out, background 0.16s ease-in-out;
       padding: 1rem;
       text-decoration: none;
+    }
 
-      &:hover {
-        color: #1f00cc;
-      }
+    &.dark {
+      background: var(--dark-background-lighter);
 
-      &:active {
-        color: #12007a;
-      }
+      a {
+        background: var(--dark-background-lighter);
+        color: var(--dark-background-color-lighter);
 
-      &.active {
-        background: #fff;
-        color: #000;
+        &.active {
+          background: var(--dark-background);
+          color: var(--dark-background-color);
+        }
       }
     }
+
+    &:not(.dark) {
+      a {
+        background: #eeebff;
+        color: #2600ff;
+
+        &:hover {
+          color: #1f00cc;
+        }
+
+        &:active {
+          color: #12007a;
+        }
+
+        &.active {
+          background: #fff;
+          color: #000;
+        }
+      }
+    }
+  }
+
+  :global(.dark) {
+    background: var(--dark-background);
+    color: #f7f7f8;
+  }
+
+  :global(body) {
+    margin: 0;
+    transition: color 0.16s ease-in-out, background 0.16s ease-in-out;
+
+    --dark-background: #1f2023;
+    --dark-background-color: #f7f7f8;
+    --dark-background-lighter: #26282e;
+    --dark-background-color-lighter: #eeebff;
+    --dark-background-modal: #303136;
   }
 </style>
